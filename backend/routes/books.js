f0bf7d9da3dd60cc213b688673fc6a085fe0b8d6
Ivan <ivanbutakov000@gmail.com>
2024-02-11 21:23:33 +0500

    Implement update book functionality

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -24,8 +24,50 @@ router
 	})
 	.get(async (req, res) => {
 		try {
-			const books = await Book.find()
-			res.status(200).send(books)
+			const books = await Book.find({})
+			res.status(200).json({
+				count: books.length,
+				data: books,
+			})
+		} catch (err) {
+			console.log(err.message)
+			res.status(500).send({ message: err.message })
+		}
+	})
+
+router
+	.route('/:id')
+	.get(async (req, res) => {
+		try {
+			const book = await Book.findById(req.params.id)
+			if (!book) {
+				return res.status(404).send({ message: 'Book not found' })
+			}
+			res.status(200).json(book)
+		} catch (err) {
+			console.log(err.message)
+			res.status(500).send({ message: err.message })
+		}
+	})
+	.put(async (req, res) => {
+		try {
+			if (!req.body.title || !req.body.author || !req.body.publishYear) {
+				return res.status(400).send({
+					message: 'All fields are required: title, author, publishYear',
+				})
+			}
+			const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+				new: true,
+			})
+
+			if (!book) {
+				return res.status(404).send({ message: 'Book not found' })
+			}
+
+			return res.status(200).json({
+				message: 'Book updated successfully:',
+				data: book,
+			})
 		} catch (err) {
 			console.log(err.message)
 			res.status(500).send({ message: err.message })
